ae12ef5e85c1b6d6742e893095860d2195b94e3e
Muhammad Abdullah <mafivs51@gmail.com>
2022-06-09 00:39:10 +0500

    ufo and enemy movement with bullets from ufo

---
+++ SpaceShooter/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿
+namespace SpaceShooter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose (bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent ()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pbUFO = new System.Windows.Forms.PictureBox();
+            this.timeGameLoop = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pbUFO)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbUFO
+            // 
+            this.pbUFO.BackColor = System.Drawing.Color.Transparent;
+            this.pbUFO.BackgroundImage = global::SpaceShooter.Properties.Resources.ufoRed;
+            this.pbUFO.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pbUFO.Location = new System.Drawing.Point(278, 330);
+            this.pbUFO.Margin = new System.Windows.Forms.Padding(0);
+            this.pbUFO.Name = "pbUFO";
+            this.pbUFO.Size = new System.Drawing.Size(120, 102);
+            this.pbUFO.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pbUFO.TabIndex = 0;
+            this.pbUFO.TabStop = false;
+            // 
+            // timeGameLoop
+            // 
+            this.timeGameLoop.Enabled = true;
+            this.timeGameLoop.Interval = 30;
+            this.timeGameLoop.Tick += new System.EventHandler(this.timeGameLoop_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::SpaceShooter.Properties.Resources.STEALER___cyberpunk_puzzle_platformer;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pbUFO);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbUFO)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbUFO;
+        private System.Windows.Forms.Timer timeGameLoop;
+    }
+}
+
---
+++ SpaceShooter/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EZInput;
+
+namespace SpaceShooter
+{
+    public partial class Form1 : Form
+    {
+        List<PictureBox> pbFires = new List<PictureBox>();
+        Random rand = new Random();
+        PictureBox enemyBlue;
+        PictureBox enemyRed;
+        PictureBox enemyBlack;
+        string enemyBlueDirection = "MovingRight";
+        string enemyRedDirection = "MovingLeft";
+        //string enemyBlackDirection = "";
+        public Form1 ()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load (object sender , EventArgs e)
+        {
+            enemyBlue = createEnemy(SpaceShooter.Properties.Resources.enemyBlue5);
+            enemyRed = createEnemy(SpaceShooter.Properties.Resources.enemyRed4);
+            enemyBlack = createEnemy(SpaceShooter.Properties.Resources.enemyBlack3);
+            addControlIntoForm(enemyBlue);
+            addControlIntoForm(enemyRed);
+            addControlIntoForm(enemyBlack);
+        }
+
+        private void timeGameLoop_Tick (object sender , EventArgs e)
+        {
+            //UFO Movement
+            ufoMovement();
+            //Firing System
+            firingSystem();
+            //Firing Bullets
+            foreach (PictureBox fire in pbFires)
+            {
+                fire.Top -= 20;
+            }
+            //Removing Bullets
+            for (int i = 0 ; i < pbFires.Count ; i++)
+            {
+                if (pbFires[i].Bottom < 0)
+                {
+                    pbFires.RemoveAt(i);
+                }
+            }
+            enemyMovement(enemyBlue , ref enemyBlueDirection);
+            enemyMovement(enemyRed , ref enemyRedDirection);
+            enemyMovementIntelli(enemyBlack);
+        }
+        public void ufoMovement ()
+        {
+            if (Keyboard.IsKeyPressed(Key.RightArrow) || Keyboard.IsKeyPressed(Key.D))
+            {
+                if ((pbUFO.Left + pbUFO.Width < this.Width))
+                {
+                    pbUFO.Left += 20;
+                }
+            }
+            if (Keyboard.IsKeyPressed(Key.LeftArrow) || Keyboard.IsKeyPressed(Key.A))
+            {
+                if (pbUFO.Left > 0)
+                {
+                    pbUFO.Left -= 20;
+
+                }
+            }
+            if (Keyboard.IsKeyPressed(Key.UpArrow) || Keyboard.IsKeyPressed(Key.W))
+            {
+                if (pbUFO.Top > 0)
+                {
+                    pbUFO.Top -= 20;
+                }
+            }
+            if (Keyboard.IsKeyPressed(Key.DownArrow) || Keyboard.IsKeyPressed(Key.S))
+            {
+                if (pbUFO.Top + pbUFO.Width + 10 < this.Height)
+                {
+                    pbUFO.Top += 20;
+                }
+
+            }
+        }
+        public void firingSystem ()
+        {
+            if (Keyboard.IsKeyPressed(Key.Space))
+            {
+                PictureBox pbFire = new PictureBox();
+                Image imgRedFire = SpaceShooter.Properties.Resources.laserRed01;
+                pbFire.Image = imgRedFire;
+                pbFire.Width = imgRedFire.Width;
+                pbFire.Height = imgRedFire.Height;
+                pbFire.BackColor = Color.Transparent;
+                //Location/Point
+                System.Drawing.Point fireLocation = new System.Drawing.Point();
+                fireLocation.X = pbUFO.Left + (pbUFO.Width / 2) - 5;
+                fireLocation.Y = pbUFO.Top;
+                pbFire.Location = fireLocation;
+                addFireIntoList(pbFire);
+                addControlIntoForm(pbFire);
+
+            }
+        }
+        public void enemyMovement (PictureBox enemy , ref string enemyDirection)
+        {
+            if (enemyDirection == "MovingRight")
+            {
+                enemy.Left += 10;
+            }
+            else if (enemyDirection == "MovingLeft")
+            {
+                enemy.Left -= 10;
+            }
+            if (enemy.Left + enemy.Width > this.Width)
+            {
+                enemyDirection = "MovingLeft";
+            }
+            if (enemy.Left <= 2)
+            {
+                enemyDirection = "MovingRight";
+            }
+        }
+        public void enemyMovementIntelli (PictureBox enemy)
+        {
+            /*if (enemy.Left > pbUFO.Left)
+            {
+                enemy.Left -= pbUFO.Left;
+            }
+            else if (enemy.Left < pbUFO.Left)
+            {
+                enemy.Left += pbUFO.Left;
+            }*/
+            /* if (enemy.Left + enemy.Width > this.Width)
+             {
+                 enemyDirection = "MovingLeft";
+             }
+             if (enemy.Left <= 2)
+             {
+                 enemyDirection = "MovingRight";
+             }*/
+        }
+        public void addFireIntoList (PictureBox pbFire)
+        {
+            pbFires.Add(pbFire);
+        }
+        public void addControlIntoForm (Control item)
+        {
+            this.Controls.Add(item);
+        }
+        private PictureBox createEnemy (Image img)
+        {
+            PictureBox pbEnemy = new PictureBox();
+            int left = rand.Next(30 , this.Width); ;
+            int top = rand.Next(5 , img.Height + 20); ;
+            pbEnemy.Left = left;
+            pbEnemy.Top = top;
+            pbEnemy.Height = img.Height;
+            pbEnemy.Width = img.Width;
+            pbEnemy.BackColor = Color.Transparent;
+            pbEnemy.Image = img;
+            return pbEnemy;
+        }
+    }
+}
